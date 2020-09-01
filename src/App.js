e2669831d7232d30aaf41081492506d9627f83ef
PedroLealLopes <pedro.ml.lopes.98@gmail.com>
2020-09-01 20:39:48 +0100

    fix: added correct html to app component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="bg-bottom">
+        <img src="./images/bg-bottom.svg" alt="background lines at the bottom" />
+      </div>
+      <div class="bg-top">
+        <img src="./images/bg-top.svg" alt="background lines at the top" />
+      </div>
+
+      <div class="pricing-header-container">
+        <div class="pricing-header-title">Our Pricing</div>
+        <span class="pricing-header-content">
+          Annually
+          <span class="pricing-header-btn-bg">
+            <button class="pricing-header-btn"></button>
+          </span>
+          Monthly
+        </span>
+      </div>
+
+      <div class="content">
+        <div class="card"></div>
+        <div class="card-main"></div>
+        <div class="card"></div>
+      </div>
     </div>
   );
 }
