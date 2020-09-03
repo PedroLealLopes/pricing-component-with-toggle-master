fb1a4aab5943f92c07b51030770c16e5c7090cdd
PedroLealLopes <pedro.ml.lopes.98@gmail.com>
2020-09-03 19:44:36 +0100

    fix: destructured components for cleaner code

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,49 +1,49 @@
 import React from 'react'
 
-const Card = (props) => {
+const Card = ({isMonthly, isMain, header, price, row1, row2, row3 }) => {
   return (
-    <div className={props.isMain ? 'card card-main' : 'card'}>
-      <div className={props.isMain ? 'card-header-main' : 'card-header'}>
-        {props.header}
+    <div className={isMain ? 'card card-main' : 'card'}>
+      <div className={isMain ? 'card-header-main' : 'card-header'}>
+        {header}
       </div>
       <div className='card-price'>
         <span className='card-price-dollar'>$</span>
-        {props.isMonthly ? props.price.monthly : props.price.annually}
+        {isMonthly ? price.monthly : price.annually}
       </div>
 
       <div className='card-info-container'>
         <div>
           <hr
             className={
-              props.isMain ? 'card-info-seperator-main' : 'card-info-seperator'
+              isMain ? 'card-info-seperator-main' : 'card-info-seperator'
             }
           />
-          <div className='card-info'>{props.row1}</div>
+          <div className='card-info'>{row1}</div>
           <hr
             className={
-              props.isMain ? 'card-info-seperator-main' : 'card-info-seperator'
+              isMain ? 'card-info-seperator-main' : 'card-info-seperator'
             }
           />
         </div>
         <div>
-          <div className='card-info'>{props.row2}</div>
+          <div className='card-info'>{row2}</div>
         </div>
         <div>
           <hr
             className={
-              props.isMain ? 'card-info-seperator-main' : 'card-info-seperator'
+              isMain ? 'card-info-seperator-main' : 'card-info-seperator'
             }
           />
-          <div className='card-info'>{props.row3}</div>
+          <div className='card-info'>{row3}</div>
           <hr
             className={
-              props.isMain ? 'card-info-seperator-main' : 'card-info-seperator'
+              isMain ? 'card-info-seperator-main' : 'card-info-seperator'
             }
           />
         </div>
       </div>
 
-      <button className={props.isMain ? 'btn btn-main' : 'btn'}>
+      <button className={isMain ? 'btn btn-main' : 'btn'}>
         LEARN MORE
       </button>
     </div>
--- src/components/PricingHeader.js
+++ src/components/PricingHeader.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const PricingHeader = (props) => {
+const PricingHeader = ({handler, monthly}) => {
   return (
     <div className="pricing-header-container">
       <div className="pricing-header-title">Our Pricing</div>
       <span className="pricing-header-content">
         Annually 
         <span className='pricing-header-btn-bg'>
-          <button onClick = {props.handler} className={props.monthly ? 'pricing-header-btn' : 'pricing-header-btn pricing-header-btn-active'}></button>
+          <button onClick = {handler} className={monthly ? 'pricing-header-btn' : 'pricing-header-btn pricing-header-btn-active'}></button>
         </span>
         Monthly
       </span>
